1534873d9b4ab6e569c2eae6507d5a7751cf64ed
btidwell <brayden.tidwell@gmail.com>
2015-05-05 22:18:07 -0700

    Test game adjustments

--- core/src/com/brassanvil/bagame/TestWorld.java
+++ core/src/com/brassanvil/bagame/TestWorld.java
@@ -4,8 +4,11 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.brassanvil.bagame.map.components.AccelerationComponent;
 import com.brassanvil.bagame.map.components.PositionComponent;
+import com.brassanvil.bagame.map.components.VelocityComponent;
 import com.brassanvil.bagame.visual.components.AnimationComponent;
 import com.brassanvil.bagame.visual.components.AnimationStateComponent;
 import com.brassanvil.bagame.visual.components.TextureComponent;
@@ -19,7 +22,7 @@ public class TestWorld {
 	Animation animation;
 
 	private static final int ROWS = 5;
-	private static final int COLS = 5;
+	private static final int COLS = 6;
 	
 	public TestWorld(Engine engine) {
 		this.engine = engine;
@@ -32,6 +35,9 @@ public class TestWorld {
 		positionComponent.x = 100;
 		positionComponent.y = 100;
 		
+		AccelerationComponent accelerationComponent = new AccelerationComponent();
+		accelerationComponent.acceleration.set(10f, 0f);
+		
 		// Create the animation
 		img = new Texture("sprite-animation1.png");
 		TextureRegion[][] tmp = TextureRegion.split(img, img.getWidth()/COLS, img.getHeight()/ROWS);
@@ -43,10 +49,14 @@ public class TestWorld {
 			}
 		}
 		animation = new Animation(0.025f, frames);
+		animation.setFrameDuration(0.1f);
+		animation.setPlayMode(PlayMode.LOOP);
 		AnimationComponent animationComponent = new AnimationComponent();
 		animationComponent.animations.put(0, animation);
 		
 		testEntity.add(positionComponent);
+		testEntity.add(accelerationComponent);
+		testEntity.add(new VelocityComponent());
 		testEntity.add(new AnimationStateComponent());
 		testEntity.add(new TextureComponent());
 		testEntity.add(animationComponent);
